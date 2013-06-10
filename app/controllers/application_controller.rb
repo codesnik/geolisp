0b8bf573a29445e96a74f487604d798e77b90cc1
codesnik <aronaxis@gmail.com>
2013-06-10 22:20:48 +0400

    stupid s-expression parser

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,25 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  require 'stupid_sexpr_parser'
   def root
-    @rule = [ 'AND',
-
-      %W[ only operating_carriers AZ VE XM CT ],
-
-      [ 'OR',
-
-        %W[ first cities MOW LED SVX ],
-
-        [ 'AND',
-          %W[ first cities IEV ],
-          %W[ only cities IEV ROM MIL ]],
-
-        [ 'AND',
-          %W[ first cities TBS ],
-          %W[ only cities TBS ROM ]],
-
-        [ 'AND',
-          %W[ first cities EVN ],
-          %W[ only cities EVN ROM ]]]]
+    @rule = StupidSexprParser.new(%[
+      (AND
+        (only operating_carriers AZ VE XM CT)
+        (OR
+          (first cities MOW LED SVX)
+          (AND
+            (first cities IEV )
+            (only cities IEV ROM MIL))
+          (AND
+            (first cities TBS)
+            (only cities TBS ROM))
+          (AND
+            (first cities EVN)
+            (only cities EVN ROM))))
+    ]).parse
   end
 end
