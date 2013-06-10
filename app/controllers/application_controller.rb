b772ced5c54c6aa4b3b35b2c54d47500a8ec3eb1
codesnik <aronaxis@gmail.com>
2013-06-10 17:38:13 +0400

    попилил div.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,24 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def root
-    render text: 'works!'
+    rule = [ :AND,
+
+      [ :only, :operating_carriers, %[AZ VE XM CT]],
+
+      [ :OR,
+
+        [ :first, :cities, %[MOW LED SVX]],
+
+        [ :AND,
+          [ :first, :cities, %[IEV]],
+          [ :only, :cities, %[IEV ROM MIL]]],
+
+        [ :AND,
+          [ :first, :cities, %[TBS]],
+          [ :only, :cities, %[TBS ROM]]],
+
+        [ :AND,
+          [ :first, :cities, %[EVN]],
+          [ :only, :cities, %[EVN ROM]]]]]
   end
 end
