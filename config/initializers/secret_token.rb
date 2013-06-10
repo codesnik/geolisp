b6ee708bc8c9e1b83494f7554783d2faf81334d5
codesnik <aronaxis@gmail.com>
2013-06-10 17:23:09 +0400

    лень было делать синатру

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geolisp::Application.config.secret_token = '1b0db82a70d81fb15e897f02533e2ea995f1e242570649a4d209ee002b63a55d3c4590a70cac5e0cc8aab75a1e856ff006dd0c2e270e4cbc7078cd32469a5f30'
