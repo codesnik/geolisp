dafd7cb84b109905951fd913cd626b31556a0435
codesnik <aronaxis@gmail.com>
2013-06-10 15:43:56 +0400

    перенос функции в заголовок

--- div.rb
+++ div.rb
@@ -26,56 +26,73 @@ template = Haml::Engine.new(<<END)
 !!! xhtml
 %html
   %head
-    %style
-      .logic {
-      padding: 5px;
-      padding-left: 30px;
-      margin: 5px;
-      border: solid 1px lightblue;
-      border-radius: 5px;
+    :css
+      .logic, .function {
+        padding: 5px;
+        padding-left: 30px;
+        margin: 5px;
+        border: solid 1px lightblue;
+        border-radius: 5px;
+      }
+      .drop-target {
       }
       label {
-      background: lightblue;
-      border: solid 1px blue;
-      border-radius: 5px;
-      margin: 5px;
-      margin-left: -25px;
-      padding: 2px;
-      cursor: move;
+        background: lightblue;
+        border: solid 1px blue;
+        border-radius: 5px;
+        margin: 5px;
+        margin-left: -25px;
+        padding: 2px;
+        cursor: move;
       }
   %body
     .logic
       %label AND
-      %div
-        %label only
-        operating_carriers
-        %input(type="text" value="AZ VE XM CT")
-      .logic
-        %label OR
-        %div
-          %label first
-          cities
-          %input(type="text" value="MOW LED SVX")
+      .drop-target
+        .function
+          %label operating_carriers
+          only
+          %input(type="text" value="AZ VE XM CT")
         .logic
-          %label AND
-          %div
-            %label first
-            cities
-            %input(type="text" value="IEV")
-          %div
-            %label only
-            cities
-            %input(type="text" value="IEV ROM MIL")
-        .logic
-          %label AND
-          %div
-            %label first
-            cities
-            %input(type="text" value="TBS")
-          %div
-            %label only
-            cities
-            %input(type="text" value="TBS ROM")
+          %label OR
+          .drop-target
+            .function
+              %label cities
+              first
+              %input(type="text" value="MOW LED SVX")
+            .logic
+              %label AND
+              .drop-target
+                .function
+                  %label cities
+                  first
+                  %input(type="text" value="IEV")
+                .function
+                  %label cities
+                  only
+                  %input(type="text" value="IEV ROM MIL")
+            .logic
+              %label AND
+              .drop-target
+                .function
+                  %label cities
+                  first
+                  %input(type="text" value="TBS")
+                .function
+                  %label cities
+                  only
+                  %input(type="text" value="TBS ROM")
+            .logic
+              %label AND
+              .drop-target
+                .function
+                  %label cities
+                  first
+                  %input(type="text" value="EVN")
+                .function
+                  %label cities
+                  only
+                  %input(type="text" value="EVN ROM")
 END
 
 puts template.render(binding)
