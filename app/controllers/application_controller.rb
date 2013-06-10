6c736c4d9f8e0a55c6ae20716228ec680ebb0105
codesnik <aronaxis@gmail.com>
2013-06-10 19:55:04 +0400

    нарезал на партиалы

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def root
-    rule = [ 'AND',
+    @rule = [ 'AND',
 
       %W[ only operating_carriers AZ VE XM CT ],
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def render_node node
+    case node.first
+    when 'AND', 'OR', 'NOT'
+      render_logic *node
+    else
+      render_function *node
+    end
+  end
+
+  def render_logic op, *inner_nodes
+    render 'logic', op: op, inner_nodes: inner_nodes
+  end
+
+  def render_function op, op2, *values
+    render 'function', op: op, op2: op2, value: values.join(' ')
+  end
 end
